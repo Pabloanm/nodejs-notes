82ea36938060055292585d72b3128d06b0de7b82
Pabloanm <pabloanm@yahoo.com>
2022-11-02 17:33:41 -0300

    Mongo Atlas Connection

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 /*
-Localhost example - Docker
+Localhost example - Docker container
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/notesdb"; 
 */
